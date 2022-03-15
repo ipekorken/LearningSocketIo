04f9f53f180171a1ae4150011262756dd4de4102
ecembalta <74915877+ecembalta@users.noreply.github.com>
2022-03-15 11:01:15 +0300

    server update

--- socketServer/server.js
+++ socketServer/server.js
@@ -7,16 +7,23 @@ http.listen(3000);
 let count = 0;
 //----------------------------user count----------------------------//
 
+//----------------------------for don't lose color when reload----------------------------//
+let activeColor = '#f0f0f0'; //It's valid if first screen is ChangeBackground.
+//----------------------------for don't lose color when reload----------------------------//
+
 io.on('connection', socket => {
   console.log('user connected');
 
   //----------------------------user count----------------------------//
   count++;
+  socket.emit('changeBgColor', activeColor); //for don't lose color when reload
   io.emit('onlineUsers', count);
+
   //----------------------------user count----------------------------//
 
   //----------------------------change background----------------------------//
   socket.on('changeBgColor', color => {
+    activeColor = color; //for don't lose color when reload
     socket.broadcast.emit('changeBgColor', color);
   });
   //----------------------------change background----------------------------//
